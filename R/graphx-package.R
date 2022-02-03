a25b7d68357ffea5430e4dd78a41f8b6e98662a1
obreschkow <danail.obreschkow@gmail.com>
2022-02-03 14:46:08 +0800

    first commit

---
+++ R/graphx-package.R
@@ -0,0 +1,9 @@
+#' @title Graphics Routines for Scientific Research
+#'
+#' @description Routines to produce and export publication-ready scientific plots and movies (e.g., used in Obreschkow et al. (2020) <doi:10.1093/mnras/staa445>). These include special color scales, projection routines, and bitmap handling routines.
+#'
+#' @author Danail Obreschkow
+#'
+#' @docType package
+#' @name graphx
+NULL
